c3df8eea04491f429bf689ec7c2ec7743d9178dd
Roberto Hidalgo <un@rob.mx>
2021-07-25 22:14:25 -0500

    Roberto/fix/document utils (#9)
    * Document utils
    
    also, bunch of a11y improvements (thank you @jeduan!)
    
    * add label to menu toggle button

--- .milpa/util/repo.sh
+++ .milpa/util/repo.sh
@@ -15,6 +15,7 @@
 function @milpa.repo.current_path () {
   repo_path="$(pwd)"
   while [[ ! -d "$repo_path/.milpa" ]]; do
+    [[ "$repo_path" == "/" ]] && return 2
     repo_path=$(dirname "$repo_path")
   done
   echo "$repo_path"
