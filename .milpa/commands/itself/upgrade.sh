b27fd4ee991e55a88cc4cd3fe3a23dd368e8c326
Roberto Hidalgo <un@rob.mx>
2021-11-02 09:19:58 -0600

    try harder to release

--- .milpa/commands/itself/upgrade.sh
+++ .milpa/commands/itself/upgrade.sh
@@ -2,7 +2,7 @@
 @milpa.load_util milpa-version
 
 if versions="$(@milpa.version.is_latest)"; then
-  @milpa.log fail "Already running latest version"
+  @milpa.fail "Already running latest version"
 fi
 
 read -r latest installed <<<"$versions"
