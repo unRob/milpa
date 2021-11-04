5af27a49ebd4a50ce5e39f9b1a12b06b0da59bbc
Roberto Hidalgo <un@rob.mx>
2021-11-03 23:06:15 -0600

    fix release create

--- repos/internal/commands/release/create.sh
+++ repos/internal/commands/release/create.sh
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+@milpa.load_util user-input
 
 current_branch=$(git rev-parse --abbrev-ref HEAD)
 [[ "$current_branch" != "main" ]] && @milpa.fail "Refusing to release on branch <$current_branch>"
