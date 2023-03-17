0c58dfd981ffa999618567ef78c029cf66385a53
Roberto Hidalgo <un@rob.mx>
2023-03-16 20:22:17 -0600

    bump to go 1.20 (#88)
    * bump to go 1.20
    
    add coverage reports from integration tests + `milpa itself doctor`

--- repos/internal/commands/dev/test.sh
+++ repos/internal/commands/dev/test.sh
@@ -3,5 +3,14 @@
 # Copyright © 2021 Roberto Hidalgo <milpa@un.rob.mx>
 
 set -e
-milpa dev test unit
-milpa dev test integration
+if [[ "$MILPA_OPT_COVERAGE" ]]; then
+  rm -rf test/coverage
+  milpa dev build --coverage
+  milpa dev test unit --coverage
+  milpa dev test integration --coverage
+  milpa dev test coverage-report
+  milpa dev build
+else
+  milpa dev test unit
+  milpa dev test integration
+fi
---
+++ repos/internal/commands/dev/test/coverage-report.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+
+runs=()
+while IFS=  read -r -d $'\0'; do
+    runs+=("$REPLY")
+done < <(find test/coverage -type d -maxdepth 1 -mindepth 1 -print0)
+packages="$(IFS=, ; echo "${runs[*]}")"
+
+
+@milpa.log info "Building coverage report from runs: ${runs[*]}"
+go tool covdata textfmt -i="$packages" -o test/coverage.cov || @milpa.fail "could not merge runs"
+go tool cover -html=test/coverage.cov -o test/coverage.html || @milpa.fail "could not build reports"
+
+@milpa.log complete "Coverage report built"
+go tool covdata percent -i="$packages"
