90db47b288b8456848bd6ae826204b5f23fb753e
Roberto Hidalgo <un@rob.mx>
2023-03-16 20:17:01 -0600

    cleanup after coverage

--- repos/internal/commands/dev/test.sh
+++ repos/internal/commands/dev/test.sh
@@ -4,10 +4,12 @@
 
 set -e
 if [[ "$MILPA_OPT_COVERAGE" ]]; then
+  rm -rf test/coverage
   milpa dev build --coverage
   milpa dev test unit --coverage
   milpa dev test integration --coverage
   milpa dev test coverage-report
+  milpa dev build
 else
   milpa dev test unit
   milpa dev test integration
