9158b4e6a21428ff05b7efa5a3d0508ced79f938
Roberto Hidalgo <un@rob.mx>
2023-03-16 19:59:55 -0600

    print report at end of run

--- repos/internal/commands/dev/test/coverage-report.sh
+++ repos/internal/commands/dev/test/coverage-report.sh
@@ -5,7 +5,10 @@ runs=()
 while IFS=  read -r -d $'\0'; do
     runs+=("$REPLY")
 done < <(find test/coverage -type d -maxdepth 1 -mindepth 1 -print0)
+packages="$(IFS=, ; echo "${runs[*]}")"
+
 
 @milpa.log info "Building coverage report from runs: ${runs[*]}"
-go tool covdata textfmt -i="$(IFS=, ; echo "${runs[*]}")" -o test/coverage.cov || @milpa.fail "could not merge runs"
+go tool covdata textfmt -i="$packages" -o test/coverage.cov || @milpa.fail "could not merge runs"
+go tool covdata percent -i="$packages"
 go tool cover -html=test/coverage.cov -o test/coverage.html || @milpa.fail "could not build reports"
